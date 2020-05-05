2e6add82deb3352bd022f1471c2189665c7befc3
tmd-uk <admin@thinkmilkdesign.co.uk>
2020-05-05 21:54:42 +0100

    Setup uikit plugins for Nuxt

--- plugins/uikit.js
+++ plugins/uikit.js
@@ -1,1 +1,10 @@
 // Setup of UIKit with Nuxt
+import Vue from 'vue'
+
+import UIKit from 'uikit/dist/js/uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+
+UIKit.use(Icons)
+UIKit.container = '#__nuxt'
+
+Vue.prototype.$uikit = UIKit
