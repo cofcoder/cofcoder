54bacd321101d7b44479d18a1f1499603d854c8c
cofcoder <shane.geach@gmail.com>
2020-05-06 16:55:11 +0100

    Messing with h1 tag
    Signed-off-by: cofcoder <shane.geach@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="uk-container">
-    <h1 class="uk-heading-medium">This is just a header</h1>
+    <h1>This is just a header</h1>
   </div>
 </template>
 
 <script>
 // import Logo from '~/components/Logo.vue'
 
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-</style>
+<style></style>
