096ff1e58b9135a9abd9f1e9505952f2105fb6e8
cofcoder <shane.geach@gmail.com>
2020-05-07 17:09:13 +0100

    Added basic navbar
    Signed-off-by: cofcoder <shane.geach@gmail.com>

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="uk-navbar uk-navbar-container uk-margin uk-navbar--custom">
+    <div class="uk-navbar-left">
+      <nuxt-link to="/" class="uk-navbar-item uk-logo">
+        <Logo />
+      </nuxt-link>
+    </div>
+    <div class="uk-navbar-right">
+      <a class="uk-navbar-toggle" href="#">
+        <span uk-navbar-toggle-icon></span>
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Logo from "@/components/Logo";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
