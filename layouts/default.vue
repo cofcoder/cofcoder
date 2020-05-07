fcc09c83a88b26485b77a4aca63403fbdbe8e1e7
cofcoder <shane.geach@gmail.com>
2020-05-07 17:09:29 +0100

    Added navbar component
    Signed-off-by: cofcoder <shane.geach@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
+    <Navbar />
     <nuxt />
   </div>
 </template>
 
-<style>
-</style>
+<script>
+import Navbar from '~/components/Navbar'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
